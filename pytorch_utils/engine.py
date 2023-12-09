94c8d6ad48ba94725847a77b138ea127e6910208
Miguel de Campos <mfcampos95@gmail.com>
2023-12-09 11:27:55 +0100

    Fix

--- pytorch_utils/engine.py
+++ pytorch_utils/engine.py
@@ -92,7 +92,7 @@ def evaluate(model, data_loader, device):
     torch.set_num_threads(1)
     cpu_device = torch.device("cpu")
     model.eval()
-    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger = pytorch_utils.utils.MetricLogger(delimiter="  ")
     header = "Test:"
 
     coco = get_coco_api_from_dataset(data_loader.dataset)
